6c04b71b1892f67480bedbf955741ff7fecd93f0
Gianluca Benassi <benox1199@gmail.com>
2022-02-17 17:10:54 +0100

    Category delete

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -30,17 +30,17 @@
                             <button type="button" class="btn btn-danger ml-auto" data-toggle="modal" data-target="#deleteModal-{{$category->id}}">Delete</button>
 
                             {{-- delete modal --}}
-                            {{-- <div class="modal fade" id="deleteModal-{{$category->id}}" tabindex="-1">
+                            <div class="modal fade" id="deleteModal-{{$category->id}}" tabindex="-1">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title">Are you sure to delete <strong>{{$post->title}}</strong>?</h5>
+                                            <h5 class="modal-title">Are you sure to delete <strong>{{$category->name}}</strong>?</h5>
                                             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                            delete form
-                                            <form action="{{route('posts.destroy', $post->id)}}" method="POST">
+                                            {{-- delete form --}}
+                                            <form action="{{route('categories.destroy', $category->id)}}" method="POST">
                                                 @csrf
                                                 @method('DELETE')
                                                 <input type="submit" class="btn btn-danger" value="Delete">
@@ -48,7 +48,7 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div> --}}
+                            </div>
 
                         </td>
                     </tr>
